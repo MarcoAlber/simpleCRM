32f604314c92a3c9d368407dc185818081a3e4cb
Marco Alber <m.alber@onlinehome.de>
2023-09-05 14:20:35 +0200

    updated saveUser function

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { User } from 'src/models/user.class';
-import { Firestore, collection, setDoc, doc } from '@angular/fire/firestore';
+import { Firestore, setDoc, doc } from '@angular/fire/firestore';
+import { collection } from 'firebase/firestore';
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -15,7 +16,7 @@ export class DialogAddUserComponent {
 
   saveUser() {
     this.user.dateOfBirth = this.dateOfBirth.getTime();
-    console.log('current User is', this.user);
+    console.log('current User is', this.user.dateOfBirth);
 
     const userCollection = collection(this.firestore, 'users');
     setDoc(doc(userCollection), this.user.toJSON());
